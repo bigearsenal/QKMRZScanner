1115b0cd841954950cbe3431b67fcbf72b67f8bf
Chung Tran <bigearsenal@gmail.com>
2025-01-17 17:24:20 +0700

    feat: some fixes

--- Sources/QKMRZScanner/SwiftUI/ViewRepresentable.swift
+++ Sources/QKMRZScanner/SwiftUI/ViewRepresentable.swift
@@ -2,44 +2,6 @@ import QKMRZParser
 import SwiftUI
 import UIKit
 
-struct MRZScannerViewRepresentable: UIViewRepresentable {
-    class Coordinator: NSObject, QKMRZScannerViewDelegate, ObservableObject {
-        fileprivate var parent: MRZScannerViewRepresentable?
-        fileprivate var scannerView: QKMRZScannerView?
-
-        func mrzScannerView(_: QKMRZScannerView, didFind scanResult: QKMRZScanResult) {
-            parent?.didFindScanResult(scanResult)
-        }
-
-        func startScanning() {
-            scannerView?.startScanning() // Assuming startScanning() is a method of QKMRZScannerView
-        }
-
-        func stopScanning() {
-            scannerView?.stopScanning() // Assuming stopScanning() is a method of QKMRZScannerView
-        }
-    }
-
-    let coordinator: Coordinator
-    var didFindScanResult: (QKMRZScanResult) -> Void
-
-    func makeCoordinator() -> Coordinator {
-        coordinator.parent = self
-        return coordinator
-    }
-
-    func makeUIView(context: Context) -> QKMRZScannerView {
-        let mrzScannerView = QKMRZScannerView()
-        mrzScannerView.delegate = context.coordinator
-        context.coordinator.scannerView = mrzScannerView // Store the reference to the scanner view
-        return mrzScannerView
-    }
-
-    func updateUIView(_: QKMRZScannerView, context _: Context) {
-        // Perform updates to the view if necessary
-    }
-}
-
 public struct MRZPassportView: View {
     @State private var sizeToFit: CGSize?
     @StateObject private var coordinator = MRZScannerViewRepresentable.Coordinator()
@@ -87,24 +49,77 @@ public struct MRZPassportView: View {
     }
 }
 
-struct SizePreferenceKey: PreferenceKey {
+private struct MRZScannerViewRepresentable: UIViewRepresentable {
+    class Coordinator: NSObject, QKMRZScannerViewDelegate, ObservableObject {
+        fileprivate var parent: MRZScannerViewRepresentable?
+        fileprivate var scannerView: QKMRZScannerView?
+
+        func mrzScannerView(_: QKMRZScannerView, didFind scanResult: QKMRZScanResult) {
+            parent?.didFindScanResult(scanResult)
+        }
+
+        func startScanning() {
+            scannerView?.startScanning() // Assuming startScanning() is a method of QKMRZScannerView
+        }
+
+        func stopScanning() {
+            scannerView?.stopScanning() // Assuming stopScanning() is a method of QKMRZScannerView
+        }
+    }
+
+    let coordinator: Coordinator
+    var didFindScanResult: (QKMRZScanResult) -> Void
+
+    func makeCoordinator() -> Coordinator {
+        coordinator.parent = self
+        return coordinator
+    }
+
+    func makeUIView(context: Context) -> QKMRZScannerView {
+        let mrzScannerView = QKMRZScannerView()
+        mrzScannerView.delegate = context.coordinator
+        context.coordinator.scannerView = mrzScannerView // Store the reference to the scanner view
+        return mrzScannerView
+    }
+
+    func updateUIView(_: QKMRZScannerView, context _: Context) {
+        // Perform updates to the view if necessary
+    }
+}
+
+private struct SizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize = .zero
     static func reduce(value _: inout CGSize, nextValue _: () -> CGSize) {}
 }
 
 private struct MockView: View {
+    @State private var result: QKMRZScanResult?
+
     var body: some View {
         VStack {
             Spacer()
             camera
-            Text("Hello")
+            VStack {
+                Text("Given Names: \(result?.givenNames ?? "No result")")
+                Text("Surnames: \(result?.surnames ?? "No result")")
+                Text("Document Type: \(result?.documentType ?? "No result")")
+                Text("Country Code: \(result?.countryCode ?? "No result")")
+                Text("Document Number: \(result?.documentNumber ?? "No result")")
+                Text("Nationality Country Code: \(result?.nationalityCountryCode ?? "No result")")
+//                Text("Birthdate: \(result?.birthdate?.formatted() ?? "No result")")
+                Text("Sex: \(result?.sex ?? "No result")")
+//                Text("Expiry Date: \(result?.expiryDate?.formatted() ?? "No result")")
+                Text("Personal Number: \(result?.personalNumber ?? "No result")")
+                Text("Personal Number 2: \(result?.personalNumber2 ?? "No result")")
+            }
+            Text(result?.givenNames ?? "No result")
             Spacer()
         }
     }
 
     var camera: some View {
         MRZPassportView { scanResult in
-            print("Scan result: \(scanResult)")
+            result = scanResult
         }
     }
 }
